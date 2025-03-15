7949448cc1d4f90ba3adb122e995856965fe467f
plaffyyy <k.zimin@innopolis.university>
2025-03-15 13:45:27 +0300

    init: initial commit

---
+++ src/main/java/ru/sos/button_project/ButtonProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.sos.button_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButtonProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ButtonProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sos/button_project/ButtonProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sos.button_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButtonProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
