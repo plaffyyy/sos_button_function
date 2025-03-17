4ce98dbd4f0799f790aa41c2ed4ce43a8444ff95
plaffyyy <k.zimin@innopolis.university>
2025-03-17 11:57:50 +0300

    feat: add prepare button function in service layer

--- src/main/java/ru/sos/button_project/repositories/SosButtonRepository.java
+++ src/main/java/ru/sos/button_project/repositories/SosButtonRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.sos.button_project.models.SosButton;
 
+import java.util.Optional;
+
 @Repository
 public interface SosButtonRepository extends JpaRepository<SosButton, Long> {
 }
--- src/main/java/ru/sos/button_project/services/SosButtonService.java
+++ src/main/java/ru/sos/button_project/services/SosButtonService.java
@@ -2,6 +2,8 @@ package ru.sos.button_project.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.sos.button_project.dto.FormRequest;
+import ru.sos.button_project.models.SosButton;
 import ru.sos.button_project.repositories.SosButtonRepository;
 
 @Service
@@ -9,6 +11,29 @@ import ru.sos.button_project.repositories.SosButtonRepository;
 public final class SosButtonService {
 
     private final SosButtonRepository sosButtonRepository;
+
+    private static final long ACTIVATION_TIMEOUT = 60; // seconds
+    private static final long ACTIVATION_COOLDOWN = 24; // hours
+
+    /**
+     * подготовка кнопки при создании, при создании кнопка готова к активации
+     * ready true, и нет последней активации
+     * @param request заполненная форма с телеграма
+     * @param userId id пользователя
+     * @return сохраненный в бд инстанс кнопки
+     */
+    public SosButton prepareButton(FormRequest request, String userId) {
+        SosButton sosButton = new SosButton();
+        sosButton.setUserId(userId);
+        sosButton.setTelegramContact(request.telegramContact());
+        sosButton.setActivationCode(request.activationCode());
+        sosButton.setMeetingAddress(request.meetingAddress());
+        sosButton.setMeetingTime(request.meetingTime());
+        sosButton.setComment(request.comment());
+        sosButton.setReady(true);
+        sosButton.setLastActivatedAt(null);
+        return sosButtonRepository.save(sosButton);
+    }
 
 
 }
